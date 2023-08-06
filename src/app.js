6f804aa736f63cd6fe4c92c83be37fe7ab3eaabd
AnastasiiaSoyluoglu <AnastasiiaSoyluoglu@users.noreply.github.com>
2023-08-06 14:25:35 +0200

    showing the current temperature

--- src/app.js
+++ src/app.js
@@ -2,14 +2,16 @@ function displayTemperature(response){
   console.log(response.data)
   let temperatureElement=document.querySelector("#temperature");
   let cityElement=document.querySelector("#city");
-  let descriptionElement=document.querySelector("#description")
-
-
+  let descriptionElement=document.querySelector("#description");
+  let humidityElement =document.querySelector("#humidity");
+  let windElemenet= document.querySelector("#wind");
 
 
   temperatureElement.innerHTML=Math.round(response.data.main.temp);
   cityElement.innerHTML= response.data.name;
   descriptionElement.innerHTML=response.data.weather[0].description;
+  humidityElement.innerHTML=response.data.main.humidity;
+  windElemenet.innerHTML= Math.round(response.data.wind.speed);
 }
 
 
