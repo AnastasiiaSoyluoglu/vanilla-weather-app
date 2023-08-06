500d266fe4792c23cc5428f46e355d4d5f995e02
AnastasiiaSoyluoglu <AnastasiiaSoyluoglu@users.noreply.github.com>
2023-08-06 15:43:40 +0200

    added search engine

--- src/app.js
+++ src/app.js
@@ -42,9 +42,21 @@ function displayTemperature(response){
   iconElement.setAttribute("alt",response.data.weather[0].description);
 }
 
-
+function search(city){
 let apiKey="81cd692b3e95836375773084a629827f";
-let city="Paris";
 let apiUrl=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
-
 axios.get(apiUrl).then(displayTemperature);
+}
+
+function handleSubmit(event){
+  event.preventDefault();
+  let cityInputElement=document.querySelector("#city-input");
+  search(cityInputElement.value);
+}
+
+search("Paris");
+
+
+let form=document.querySelector("#search-form");
+form.addEventListener("submit",handleSubmit);
+
