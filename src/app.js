a95822d100099cad7e76458ea299ba5c35f2d0ce
AnastasiiaSoyluoglu <AnastasiiaSoyluoglu@users.noreply.github.com>
2023-08-08 15:18:25 +0200

    connected api for forecast

--- src/app.js
+++ src/app.js
@@ -21,7 +21,9 @@ function formatDate(timestamp){
   return `${day}${hours}:${minutes}`; 
 }
 
-function displayForecast() {
+function displayForecast(response) {
+  console.log(response.data.daily);
+
   let forecastElement= document.querySelector("#forecast")
 
   
@@ -48,7 +50,15 @@ function displayForecast() {
  });
   forecastHTML=forecastHTML+`</div>`;
   forecastElement.innerHTML=forecastHTML;
-  console.log(forecastHTML);
+}
+
+function getForecast(coordinates){
+  console.log(coordinates);
+  let apiKey="2bd326a60dc89a53287e446e819664df";
+  let apiUrl= `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+
 }
 
 
@@ -73,6 +83,8 @@ function displayTemperature(response){
     "src",`http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt",response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city){
